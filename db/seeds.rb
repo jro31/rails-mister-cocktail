336556ec6ab227224784abb6c7a0e463d1dc6302
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-05 12:37:07 +0800

    Install the faker gem to succesfully populate the database with ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Emptying the database..."
+Ingredient.destroy_all
+puts "Generating new ingredients..."
+
+20.times do |i|
+  Ingredient.create(name: Faker::Food.dish)
+  Ingredient.create(name: Faker::Food.ingredient)
+  Ingredient.create(name: Faker::Food.fruits)
+  Ingredient.create(name: Faker::Food.vegetables)
+  Ingredient.create(name: Faker::Food.spice)
+end
+
+puts "Finished generating ingredients..."
+
+# puts "Emptying the DB"
+# Restaurant.destroy_all
+# puts "Creating restaurants"
+
+# 20.times do |i|
+#   restaurant = Restaurant.new(name: Faker::GreekPhilosophers.name, address: Faker::GameOfThrones.city, phone_number: Faker::PhoneNumber.cell_phone, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, )
+#   restaurant.save!
+#   puts "Added new restaurant"
+# end
+# puts "Finished seeding!"
