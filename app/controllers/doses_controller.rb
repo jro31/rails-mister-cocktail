585ba11806bede94c136026a956227dc1fc00661
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-05 21:06:10 +0800

    Add the ability to add ingredients with descriptions to cocktails and display them in the view, and use an if statment to verify the validity of them

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,8 +7,12 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save
-    redirect_to cocktail_path(@dose[:cocktail_id])
+    if @dose.valid?
+      @dose.save
+      redirect_to cocktail_path(@dose[:cocktail_id])
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -19,5 +23,5 @@ end
 private
 
 def dose_params
-  params.require(:dose).permit(:description)
+  params.require(:dose).permit(:description, :ingredient_id)
 end
