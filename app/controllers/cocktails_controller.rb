918bdaada29e2d2a3513189a60ead2cfc1015689
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-06 13:57:33 +0800

    Add in a splash page that users get to when they first arrive at the website

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,6 +14,7 @@ class CocktailsController < ApplicationController
   end
 
   def index
+
     @cocktails = Cocktail.all
   end
 
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def splash
+    @hide = true
+  end
+end
